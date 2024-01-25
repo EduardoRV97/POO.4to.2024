0c96e1da5c7b2bf0ff636d3adea8e4935df00ef6
EduardoRV97 <erivera39414@ucq.edu.mx>
2024-01-25 10:34:58 -0600

    Apuntadores y Herencia Simple
    Algunos ejemplos de apuntadores y Herencia Simple

--- CuentaBancaria/CuentaBancaria/Include/Cuerpo.h
+++ CuentaBancaria/CuentaBancaria/Include/Cuerpo.h
@@ -48,19 +48,54 @@ protected:
 
 class Rectangulo {
 
-public : 
+public:
 
-	Rectangulo(int altura, int base) : altura (altura), base(base) {}
+	Rectangulo(int altura, int base) : altura(altura), base(base) {}
 
 	~Rectangulo() {}
 
-private :
+private:
 	int base;
 	int altura;
 
-public :
+public:
 
 	int FormulaP() {
 		return (altura * base);
+	}
+};
+class Estudiante {
+public:
+	Estudiante(string _nombre, int _edad, float _calificacion) : m_nombre(_nombre), m_edad(_edad), m_calificacion(_calificacion) {}
+	~Estudiante() {}
+private:
+	string m_nombre;
+	int m_edad;
+	float m_calificacion;
+public:
+	void mostrarInformacion() {
+		cout << "El nombre del estudiante es " << m_nombre << " , tiene un edad de " << m_edad << " Y su calificacion es de " << m_calificacion << endl;
+	};
+};
+
+class Vehiculo {
+public:
+	Vehiculo(){}
+	Vehiculo(string _modelo, int _año) : m_modelo(_modelo), m_año(_año){}
+	~Vehiculo(){}
+protected:
+	string m_modelo;
+	int m_año;
+};
+
+class Coche : public Vehiculo {
+public:
+	Coche(string _modelo, int _año, int _puertas) : Vehiculo(_modelo, _año),m_puertas(_puertas) {}
+	~Coche(){}
+private:
+	int m_puertas;
+public:
+	void mostrarInformacionCoche() {
+		cout << "El coche del modelo " << m_modelo << " del año " << m_año << ", tiene un total de " << m_puertas << "puertas." << endl;
 	}
 };
--- CuentaBancaria/CuentaBancaria/Source/Main.cpp
+++ CuentaBancaria/CuentaBancaria/Source/Main.cpp
@@ -2,11 +2,18 @@
 // Hay 3 niveles de acceso, publicos, protegidos y privados
 // El acceso publico, son los miembros declarados como publicos son accesibles desde cualquier parte del programa
 // Acceso protegido los miembros declarados como protegidos son accesibles solo dentro de la clase y sus clase derivadas. No pueden ser accedidos desde fuera
-// Acceso privado, los miebros declarados como proivados son inaccesibles desde fuera de la clase. Solo la propia clase puede acceder a ellos.
+// Acceso privado, los miebros declarados como proivados son inaccesibles desde fuera de la clase.
+// Solo la propia clase puede acceder a ellos.
+// Pilares del uso de punteros:
+// 1.- Gestion dinamica de la memoria
+// 2.- Argumentos en metodos
+// 3.- Trabajar con estrcuturas de datos complejos
+// 4.- Eficiencia y manipulacion directa de memoria
+// 5.- Arreglos Dinamicos
 
-
-#include"Prerequisitos.h"
+#include "Prerequisitos.h"
 #include "Cuerpo.h"
+#include "Persona.h"
 
 int main() {
 	CuentaBancaria CuentaBancaria("Eduardo");
@@ -27,6 +34,42 @@ int main() {
 	int Areaa = Area.FormulaP();
 	
 	cout << "El valor del area es " << Areaa << endl;
+
+	Persona* persona = new Persona("Roberto", 25);
+	persona->print();
+
+	Persona* persona2 = new Persona(&persona[0]);
+	persona2->print();
+
+	Persona persona4 = new Persona("Jose",27);
+	persona4.print();
+
+	Persona persona3 = new Persona(persona4);
+	persona3.print();
+
+	delete persona;
+	delete persona2;
+	//delete persona3;
+
+	const int numEstudiantes = 3;
+	Estudiante** estudiantes = new Estudiante * [numEstudiantes];
+
+	estudiantes[0] = new Estudiante("Camila", 21, 90.85);
+	estudiantes[1] = new Estudiante("Pepe", 34, 76.56);
+	estudiantes[2] = new Estudiante("Eduardo", 67, 67.56);
+
+	for (int i = 0; i < numEstudiantes; ++i) {
+		estudiantes[i]->mostrarInformacion();
+	}
+
+	for (int i = 0; i < numEstudiantes; ++i) {
+		delete estudiantes[i];
+	}
+
+	delete[] estudiantes;
+
+	Coche miCoche("Carrito", 2024, 4);
+	miCoche.mostrarInformacionCoche();
 
 	return 0;
 }
